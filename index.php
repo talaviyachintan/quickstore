f83cb3f70e859bfe6dab34097114098e806a6d53
talaviyachintan <140591674+talaviyachintan@users.noreply.github.com>
2024-12-15 11:12:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="0; url=https://example.com/new-location" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quickstore - Electronics Shop</title>
-    
+    <script type="text/javascript">
+        window.location.href = "https://talaviyachintan.github.io/quickstore/";
+    </script>
 </head>
 <body>
     <!-- start loader -->
